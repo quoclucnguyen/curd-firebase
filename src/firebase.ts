a23ad032bc1ca2c37e07504413469e41884a2095
Luke Nguyen <quocluc97@gmail.com>
2024-05-26 13:27:07 +0700

    refactor: Update firebase.ts to improve code organization and readability

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { schema, Typesaurus } from "typesaurus";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,12 +16,11 @@ const app = initializeApp(firebaseConfig);
 export default app;
 
 export const db = schema(($) => ({
-  
-  users: $.collection<User>().sub({
-    notes: $.collection<Note>(),
+  users: $.collection<User>(),
+  items: $.collection<Item>(),
+  itemTypes: $.collection<ItemType>().sub({
+    items: $.collection<Item>(),
   }),
-  orders: $.collection<Order>(),
-  books: $.collection<Book>(),
 }));
 
 export type Schema = Typesaurus.Schema<typeof db>;
@@ -32,15 +31,14 @@ interface User {
   name: string;
 }
 
-interface Note {
-  text: string;
+
+interface ItemType {
+  name: string;
 }
 
-interface Order {
-  userId: Schema["users"]["Id"];
-  bookId: Schema["books"]["Id"];
-}
-
-interface Book {
-  title: string;
+interface Item {
+  name: string;
+  type: ItemType;
+  imageUrl?: string;
+  itemTypeId: Schema["itemTypes"]["Id"];
 }
